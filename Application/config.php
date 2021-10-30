502f22c4d06f07ea57bab3690ec4d769afe0eeb9
yeum <conoist@.naver.com>
2021-10-31 03:25:28 +0900

    mysql 연동 완료

---
+++ Application/config.php
@@ -0,0 +1,6 @@
+<?php
+return array(
+    "dsn" => "mysql:host=10.200.19.250;port=33060;dbname=game;charset=utf8",
+    "dbUserName" => "root",
+    "dbPassword" => "password"
+);
--- Application/index.php
+++ Application/index.php
@@ -1,4 +1,27 @@
 <?php
-echo 'project LIME!';
-echo phpinfo();
+echo 'project LIME!'."<br>"."<br>";
+$host = "localhost";
+$user = "root";
+$pw = "password";
+$dbName = "game";
+
+try{
+    $config = include('./config.php');
+
+    $conn = new PDO($config['dsn'], $config['dbUserName'], $config['dbPassword']);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+    /* SELECT 예제 */
+    $sql = "SELECT * FROM `TB_USER`";
+    $result = $conn->prepare($sql);
+    $result->execute();
+
+    echo "[DB TEST START]"."<br>";
+    while($row = $result->fetch(PDO::FETCH_ASSOC)){
+        echo "user_id: " . $row['user_id'] . ", nickname: " . $row['nickname'] . "<br>";
+    }
+} catch(PDOException $e) {
+    die( 'MySQL 연결실패 : ' . $e->getMessage());
+}
 ?>
